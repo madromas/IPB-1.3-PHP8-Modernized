f2fe646117763b49847d0d27d25e38b6ad92a595
MadWay Inc. <64708726+madromas@users.noreply.github.com>
2026-05-15 14:14:36 -0400

    Add files via upload

--- sources/Topics.php
+++ sources/Topics.php
@@ -358,25 +358,38 @@ else
         	$std->Error( array( 'LEVEL' => 1, 'MSG' => 'no_view_topic') );
         }      
         
-        //-------------------------------------
-        // Update the topic views counter
-        //-------------------------------------
-        
-        $DB->query("UPDATE ibf_topics SET views=views+1 WHERE tid='".$this->topic['tid']."'");
-        
-        //-------------------------------------
-        // Update the topic read cookie
-        //-------------------------------------
-        
-        if ($ibforums->member['id'])
-{
-    $current_read = @unserialize(stripslashes($std->my_getcookie('topicsread') ?? ''));
+      
+//-------------------------------------
+// Update the topic views counter (Unique Fix)
+//-------------------------------------
 
-if (!is_array($current_read)) {
-    $current_read = array();
+if ( ! strstr( $std->my_getcookie('topics_read'), ",".$this->topic['tid']."," ) )
+{
+    $DB->query("UPDATE ibf_topics SET views=views+1 WHERE tid='".$this->topic['tid']."'");
+    $std->my_setcookie('topics_read', $std->my_getcookie('topics_read').",".$this->topic['tid'].",");
 }
+      
+        //-------------------------------------
+// Update the topic read cookie
+//-------------------------------------
+
+if ($ibforums->member['id'])
+{
+    $raw_cookie = $std->my_getcookie('topicsread');
+    $current_read = $raw_cookie ? @unserialize(stripslashes($raw_cookie)) : array();
+
+    if (!is_array($current_read)) 
+    {
+        $current_read = array();
+    }
     
     $current_read[$this->topic['tid']] = time();
+    
+    if (count($current_read) > 40)
+    {
+        asort($current_read);
+        $current_read = array_slice($current_read, -40, 40, true); 40
+    }
     
     $std->my_setcookie('topicsread', serialize($current_read), 1);
 }
